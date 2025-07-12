6f13fd58a99156f4b0828d57e6ced7ca6225b02a
Vaibhav <krvaibhav9999@gmail.com>
2025-07-12 07:52:34 +0530

    First commit

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  
+    app.config['SECRET_KEY'] = "ENTER SOME KEY"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    
+    # Initialize extensions
+    db.init_app(app)
+    
+    # Import models (important for migrations)
+    # from app.models import ...
+    
+    # Import Routes
+    # eg: from .routes.home import home
+
+    # Register Routes
+    # eg: app.register_blueprint(home, url_prefix='/')
+
+
+
+
+    migrate = Migrate(app, db)
+    
+    return app
---
+++ run.py
@@ -0,0 +1,7 @@
+from app import create_app
+
+
+myapp = create_app()
+
+if __name__ == '__main__':
+    myapp.run(debug=True)
